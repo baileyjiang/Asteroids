975cad39e3ef8dfd79435e8508ca9e710f365dd1
b@ileyjiang <b@ileyjiang.com>
2016-12-18 00:36:18 -0700

    New activity for menu

--- app/src/main/java/cs4530/asteroids/MainActivity.java
+++ app/src/main/java/cs4530/asteroids/MainActivity.java
@@ -1,6 +1,12 @@
 package cs4530.asteroids;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by blyjng on 12/17/16.
@@ -8,6 +14,19 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
 
-
+        Button startGame = (Button) findViewById(R.id.start_game);
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newGameIntent = new Intent();
+                newGameIntent.setClass(MainActivity.this, GameActivity.class);
+                startActivity(newGameIntent);
+            }
+        });
+    }
 }
